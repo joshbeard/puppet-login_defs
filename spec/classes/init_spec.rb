accf4f02696bf8925ae9060c83d6ab69e816d51d
Josh Beard <josh@joshbeard.me>
2024-02-06 17:11:27 -0700

    Deprecate common_options; Simplify data
    Deprecate the `common_options` parameter, which was a way to deal with
    selectively merging configuration options.
    This simplifies the data a bit to make the merge behavior less
    confusing.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -15,6 +15,35 @@ describe 'login_defs' do
       params['options'].sort.each { |k, v| file += "#{k} #{v}\n" }
 
       it { is_expected.to contain_file('/etc/login.defs').with_content(file) }
+
+      it { is_expected.not_to contain_notify('common_options_deprecated') }
     end
+  end
+
+  context 'with deprecated common_options parameter' do
+    let(:facts) do
+      {
+        osfamily: 'RedHat',
+        operatingsystem: 'RedHat',
+        operatingsystemrelease: '7.0',
+        operatingsystemmajrelease: '7',
+        architecture: 'x86_64',
+        path: '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin',
+        kernel: 'Linux',
+        selinux: true,
+        is_virtual: false
+      }
+    end
+    let(:params) do
+      {
+        common_options: {
+          'UMASK' => '077',
+        }
+      }
+    end
+
+    it { is_expected.to contain_file('/etc/login.defs').with_content(CONF_HEADER + "UMASK 077\n") }
+
+    it { is_expected.to contain_notify('common_options_deprecated') }
   end
 end
